bff4adbf92cdc54167e1a0c3e771c38d3c352400
DrakeDev23 <maccogoth@gmail.com>
2026-09-26 15:38:47 +0800

    updated backend

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -7,9 +7,7 @@ use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends Factory<Task>
- */
+
 class TaskFactory extends Factory
 {
     protected $model = Task::class;
